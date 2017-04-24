f71998209743d57a141a4171a2a47892b7b881ff
Yusuke Haneda <yusukehaneda@gmail.com>
2017-04-24 14:30:35 +0000

    test_presense_and_rength

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
+# プレゼンス（存在性）がtrueであること。空白ではダメ
+#　マキシマム50 最大50文字
