2b56840cec1b1c1a4784f228aa0c03c76644fbe7
Yusuke Haneda <yusukehaneda@gmail.com>
2017-04-24 15:04:29 +0000

    db_unique_after_6.2.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
 # プレゼンス（存在性）がtrueであること。空白ではダメ
+#　VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i　は正規表現にのっとっているかを確認するコード
 #　マキシマム50 最大50文字
+# format: { with: VALID_EMAIL_REGEX }オプション上のVALID_EMAIL_REGEXを指定している
+#case_sensitive 小文字と大文字を区別しない
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,12 +11,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @user.name = ""
+    @user.name = ""  
     assert_not @user.valid?
   end
 
   test "email should be present" do
-    @user.email = "     "
+    @user.email = "     "   #空欄はダメ
     assert_not @user.valid?
   end
 
@@ -29,5 +29,33 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+   test "email validation should accept valid addresses" do
+     #正解のデータ
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+                        #アンダーバーもハイフンも大文字もOK＠以前でドットもOK＠以前であればプラスが使える
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+      #↑これをいれておくとどこで失敗したかがわかる
+    end
+  end
   
+  test "email validation should reject invalid addresses" do #はじきましょうというテスト
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+                               #ピリオドの代わりにカンマになっている　＠のあとアンダーバーはダメとか
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase  #upcaseは小文字を大文字にする
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
