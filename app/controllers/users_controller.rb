7b495c747c72f7e34df038799d7e23a4128560e0
Yusuke Haneda <yusukehaneda@gmail.com>
2017-04-28 13:30:26 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #success
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
     else
       #failure
     render 'new'
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } } #postリクエストを送るテストで使えるメソッド。間違えるに違いない
+    end
+    assert_template 'users/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } } #成功するテストの内容を入れる
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
