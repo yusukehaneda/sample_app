5d8853093222b35ce06679a5034f0446cb069aef
Yusuke Haneda <yusukehaneda@gmail.com>
2017-04-29 06:53:57 +0000

    Finsh ch07-02

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,42 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id]) #URLのidと同じデータを引っ張る（user/2というURLにしたらid=2のユーザを引っ張る）
-    # debugger
+    @user = User.find(params[:id]) 
   end
-#showアクションは　app/views/users/show.html.erbを呼び出す
+
+#def showはshouwアクションのこと。これは　app/views/users/show.html.erbを呼び出す
+#URLのidと同じデータを引っ張る（user/2というURLにしたらid=2のユーザを引っ張る）
+# 例えばUser.firstとやるとユーザid=1の人が出てくる
+# user = User.firstというふうにすると、このdef showアクション以外では使用できないので、@user = User.～としている
+# debugger
 
   def new
     @user = User.new #空のインスタンスをまず作成する
   end
   
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params) 
+    #この下にuser_paramsというメソッドを作る↓↓
+    #こうするとすべてのuser属性が当てはまっていく。一行で済む
+    # @user = User.new(params[:user])とするとadminとかで登録できてしまう。マスアサイメント脆弱性
     if @user.save
       #success
-        flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Sample App!" #flashというメソッド（変数）は一度だけ出して、二度目は消す
         redirect_to @user
+        #userのプロフィールページに飛ぶ redirect_to user_path(@user.id)の省略形
+        #GET / users/:idという処理が行われる
     else
+      render 'new'
       #failure
-    render 'new'
+      #newテンプレートをレンダリングする。つまり、その中身が何であれ、signアップフォームに戻す
     end
   end
   
-  def user_params
+  private
+    def user_params #def createの子メソッド
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
 end
 
 #signupのページ
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,6 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase) #メールアドレスを散らしたもの
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar") #ソースはグラバターの画像
+    image_tag(gravatar_url, alt: user.name, class: "gravatar") #ソースはグラバターの画像 htmlのクラスをgravaterとしている
   end
 end
