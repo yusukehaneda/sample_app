821aa66ff40ee1e218e466c827a9c4e937e8cd71
Yusuke Haneda <yusukehaneda@gmail.com>
2017-04-29 07:03:30 +0000

    gravatar_img_size_change

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
 
   # 引数で与えられたユーザーのGravatar画像を返す 引数にuserと書いてある
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase) #メールアドレスを散らしたもの
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar") #ソースはグラバターの画像 htmlのクラスをgravaterとしている
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar") 
+    #ソースはグラバターの画像 htmlのクラスをgravaterとしている
   end
 end
