0b82d77d2ada924515a53496d6d5aa80d2a614de
Yusuke Haneda <yusukehaneda@gmail.com>
2017-05-20 14:42:42 +0000

    AWS3_syusei

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,23 +1,21 @@
-# encoding: utf-8
-
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
-  
+
   if Rails.env.production?
     storage :fog
   else
     storage :file
   end
 
-  storage :file
+  # アップロードファイルの保存先ディレクトリは上書き可能
+  # 下記はデフォルトの保存先  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
-    # アップロード可能な拡張子のリスト
+
+  # アップロード可能な拡張子のリスト
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
 end
