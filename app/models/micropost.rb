f11462b46fbe4879c0945c261030cd5cb4255e0d
Yusuke Haneda <yusukehaneda@gmail.com>
2017-05-20 13:38:27 +0000

    ch13_beforeAWS_S3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,18 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
+  # -> @micropost.user.name
+  default_scope -> { order(created_at: :desc) }#降順に並べる
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 3.megabytes
+        errors.add(:picture, "should be less than 3MB")
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy  #userとmicropostとの関連付け
   before_save { self.email = self.email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -15,6 +15,12 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
 end
