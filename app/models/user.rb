6885a82f0535a47553f510e9de403a891c008a4c
Yusuke Haneda <yusukehaneda@gmail.com>
2017-05-29 14:57:02 +0000

    ch14_finish_2

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
                         length: { minimum: 6 },
                         allow_nil:true
                         
-    # 渡された文字列のハッシュ値を返す 三項演算子
+    # 渡された文字列のハッシュ値を返す 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
