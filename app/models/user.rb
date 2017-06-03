d9069206e765fedea61f696cabbaede6e8cf9124
Yusuke Haneda <yusukehaneda@gmail.com>
2017-06-03 08:24:41 +0000

    finish_ch12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
-#  before_save { self.email = self.email.downcase }
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   
@@ -78,6 +77,24 @@ class User < ApplicationRecord
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+    # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  #2時間前より早い。例えば1時間前とか30分前
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   
     # ユーザーをフォローする
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'password_resets/new'
+
+  # get 'password_resets/edit'
+
   root 'static_pages#home'
   get     '/help',     to: 'static_pages#help'
   get     '/about',    to: 'static_pages#about'
@@ -18,6 +22,7 @@ Rails.application.routes.draw do
     end
   end
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
 end
